f72c2ac3ed6ee0212fb013e80bc59522be57eb92
Navid <navidrohim@gmail.com>
2025-05-30 21:31:14 +0100

    1.21.1 rc; still yet to do tests

--- src/main/java/me/brynview/navidrohim/jmws/JMWS.java
+++ src/main/java/me/brynview/navidrohim/jmws/JMWS.java
@@ -1,4 +1,5 @@
 package me.brynview.navidrohim.jmws;
+import me.brynview.navidrohim.jmws.common.JMWSConstants;
 import me.brynview.navidrohim.jmws.common.payloads.HandshakePayload;
 import me.brynview.navidrohim.jmws.common.payloads.JMWSActionPayload;
 import net.fabricmc.api.EnvType;
@@ -7,6 +8,8 @@ import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 
+import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.api.VersionParsingException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
@@ -17,7 +20,7 @@ import java.util.Optional;
 public class JMWS implements ModInitializer {
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.1.1-1.21.5";
+    public static final String VERSION = "1.1.1-1.21.1";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
     public static void info(Object message) {
@@ -26,7 +29,7 @@ public class JMWS implements ModInitializer {
 
     private void _handleMissingMod(@Nullable Exception exc) {
         JMWS.LOGGER.error("Got error checking JM version; %s".formatted(exc));
-        throw new RuntimeException("JourneyMap might be installed, but the version cannot be detected. Need JourneyMap version 1.21.5-6.0.0-beta.47 or higher.");
+        throw new RuntimeException("JourneyMap might be installed, but the version cannot be detected. Need JourneyMap version 1.21.1-6.0.0-beta.47 or higher.");
     }
 
     @Override
@@ -41,20 +44,33 @@ public class JMWS implements ModInitializer {
                     Optional<ModContainer> jmModContainer = fabricLoader.getModContainer("journeymap");
                     if (jmModContainer.isPresent()) {
                         String versionString = jmModContainer.get().getMetadata().getVersion().getFriendlyString();
-                        if (!(Integer.parseInt(versionString.substring(versionString.length() - 2)) >= 47)) {
-                            throw new RuntimeException("JourneyMap is installed (version %s) but it is the wrong version. Need 1.21.5-6.0.0-beta.47 or higher".formatted(versionString)); // using translatable string because this could be a common error
+
+                        SemanticVersion minAllowedVersion = SemanticVersion.parse(JMWSConstants.JourneyMapVersionString);
+                        SemanticVersion betaVersion = SemanticVersion.parse(versionString);
+
+                        // JM version from jar
+                        int mcVersionMinor = betaVersion.getVersionComponent(1);
+                        int mcVersionPatch = betaVersion.getVersionComponent(2);
+                        int jmVersionBetaPatch = betaVersion.getVersionComponent(6);
+
+                        int minMcVersionMinor = minAllowedVersion.getVersionComponent(1);
+                        int minMcVersionPatch = minAllowedVersion.getVersionComponent(2);
+                        int minMcVersionBeta = minAllowedVersion.getVersionComponent(6);
+
+                        if (!(mcVersionMinor >= minMcVersionMinor && mcVersionPatch == minMcVersionPatch && jmVersionBetaPatch >= minMcVersionBeta)) {
+                            throw new RuntimeException("JourneyMap is installed (version %s) but it is the wrong version. Need %s or higher".formatted(versionString, JMWSConstants.JourneyMapVersionString)); // using translatable string because this could be a common error
                         }
                         JMWS.LOGGER.info("Good to go. JMWS Version %s with JourneyMap Version %s on client-side.".formatted(JMWS.VERSION, versionString));
                     } else {
                         _handleMissingMod(null);
                     }
                 } else {
-                    throw new RuntimeException("JourneyMap Version 6.0.0 Beta 47 or higher (1.21.5+) is required on the client-side of JMWS.");
+                    throw new RuntimeException("JourneyMap %s is required on the client-side of JMWS.".formatted(JMWSConstants.JourneyMapVersionString));
                 }
 
             }
-        } catch (NoSuchElementException noSuchElementException) {
-            _handleMissingMod(noSuchElementException);
+        } catch (NoSuchElementException | VersionParsingException exception) {
+            _handleMissingMod(exception);
         }
 
         // Packet registering (client)
--- src/main/java/me/brynview/navidrohim/jmws/common/JMWSConstants.java
+++ src/main/java/me/brynview/navidrohim/jmws/common/JMWSConstants.java
@@ -4,4 +4,5 @@ import java.util.List;
 
 public interface JMWSConstants {
     List<String> forbiddenGroups = List.of("journeymap_death", "journeymap_all", "journeymap_temp", "journeymap_default");
+    public static final String JourneyMapVersionString = "1.21.1-6.0.0-beta.47";
 }
