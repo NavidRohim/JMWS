5cdc93caa9d93b88c960cee352db5c60c66bb469
Navid <navidrohim@gmail.com>
2025-05-30 22:06:54 +0100

    merged main with 1.21.1

--- src/main/java/me/brynview/navidrohim/jmws/JMWS.java
+++ src/main/java/me/brynview/navidrohim/jmws/JMWS.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class JMWS implements ModInitializer {
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.1.1-1.21.1-RC";
+    public static final String VERSION = "1.1.1-1.21.5+001";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
     public static void info(Object message) {
