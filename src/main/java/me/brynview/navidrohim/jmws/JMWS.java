049b7617637a1acc80743c41a4f549aa84ffa2a0
Navid <navidrohim@gmail.com>
2025-06-04 20:28:29 +0100

    essential 1.21.1 1.1.3 changes

--- src/main/java/me/brynview/navidrohim/jmws/JMWS.java
+++ src/main/java/me/brynview/navidrohim/jmws/JMWS.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class JMWS implements ModInitializer {
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.1.2-1.21.1";
+    public static final String VERSION = "1.1.3-1.21.1-RC";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
     public static void info(Object message) {
