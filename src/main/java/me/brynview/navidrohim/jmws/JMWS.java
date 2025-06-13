75a7ea4d586a4712d2d8a162896ea8ca0231c611
Navid <navidrohim@gmail.com>
2025-06-13 17:09:01 +0100

    1.1.3; refer to readme for changes.

--- src/main/java/me/brynview/navidrohim/jmws/JMWS.java
+++ src/main/java/me/brynview/navidrohim/jmws/JMWS.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 public class JMWS implements ModInitializer {
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.1.3-1.21.5-RC3";
+    public static final String VERSION = "1.1.3-1.21.5";
     public static final Logger LOGGER = LogManager.getFormatterLogger(MODID);
 
     public static void info(Object message) {
