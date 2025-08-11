be7d29b42f784e84af163f4f7ebd7a3e72e2b721
Navid <navidanjumsamirohim@gmail.com>
2025-08-11 06:34:53 +0100

    trying to fix mixins

---
+++ src/main/java/me/navidrohim/jmws/client/mixin/MixinLoadingPlugin.java
@@ -0,0 +1,14 @@
+package me.navidrohim.jmws.client.mixin;
+
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class MixinLoadingPlugin implements ILateMixinLoader {
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.jmws.json");
+    }
+}
