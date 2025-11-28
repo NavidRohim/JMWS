74d6cc0280bbbe92f19951b844acaae5e601285c
Navid <navidanjumsamirohim@gmail.com>
2025-11-28 22:21:06 +0000

    updated version number

--- common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
@@ -16,7 +16,7 @@ public class Constants {
     }
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.2.0-1.21.1-beta.28"; // This is purely for display and is not needed
+    public static final String VERSION = "1.2.0-1.21.10"; // This is purely for display and is not needed
     public static final double SERVER_VERSION = 1.1;
     public static final boolean DEBUG = VERSION.contains("-beta.");
 
@@ -24,6 +24,6 @@ public class Constants {
     public static final List<String> forbiddenGroups = List.of("journeymap_death", "journeymap_all", "journeymap_temp", "journeymap_default"); // (DO NOT EDIT, game will bug out)
 
     // For anyone forking, this MUST be updated if there is a change that is in a future version of JM that you use.
-    public static final String JourneyMapVersionString = "1.21.1-6.0.0-beta.52";
+    public static final String JourneyMapVersionString = "1.21.10-6.0.0-beta.52";
     // beta 52 fixed the waypoint-drag-drop event, so this is the only version compatible (and any newer)
 }
