6e1f6437614ae7c212563bd8085bf02b1740cf46
Navid <navidanjumsamirohim@gmail.com>
2026-05-15 19:12:54 +0100

    Updated JM jar requirements for minimum server syncing

--- common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
     public static final String MODID = "jmws";
     public static final String VERSION = "1.2.6-1.21.1"; // This is purely for display and is not needed
     public static final double SERVER_VERSION = 1.13;
-    public static final int JOURNEYMAP_LOCAL_SERVER_WAYPOINTS_BETA = 71;
+    public static final int JOURNEYMAP_LOCAL_SERVER_WAYPOINTS_BETA = 67;
     public static final boolean DEBUG = VERSION.contains("-beta.");
 
     public static final List<String> allowedMods = List.of(MODID, "journeymap"); // DO NOT CHANGE!
@@ -29,7 +29,7 @@ public class Constants {
     public static final List<String> forbiddenGroups = List.of("journeymap_death", "journeymap_all", "journeymap_temp", "journeymap_default"); // (DO NOT EDIT, game will bug out)
 
     // For anyone forking, this MUST be updated if there is a change that is in a future version of JM that you use.
-    public static final String JourneyMapVersionString = "1.21.1-6.0.0-beta.58";
+    public static final String JourneyMapVersionString = "1.21.1-6.0.0-beta.67";
     // beta 52 fixed the waypoint-drag-drop event, so this is the only version compatible (and any newer)
 
     public static void updateServerJourneyMapStatus(String versionString) {
