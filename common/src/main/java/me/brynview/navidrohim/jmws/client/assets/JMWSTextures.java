71bdbe26b89cd51324783d7b5810f855be385eaf
Navid <navidanjumsamirohim@gmail.com>
2026-05-18 22:08:38 +0100

    New assets, changed wording to reflect global migration

--- common/src/main/java/me/brynview/navidrohim/jmws/client/assets/JMWSTextures.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/client/assets/JMWSTextures.java
@@ -8,9 +8,9 @@ import net.minecraft.resources.Identifier;
  */
 public interface JMWSTextures {
 
-    Identifier onOffButtonAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/on_off_button.png");
+    Identifier onOffButtonAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/toggle.png");
     Identifier enableButtonAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/sync.png");
 
-    Identifier globalObjectAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/global_object.png");
-    Identifier sharedObjectAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/shared_object.png");
+    Identifier globalObjectAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/global.png");
+    Identifier sharedObjectAsset = Identifier.fromNamespaceAndPath(Constants.MODID, "textures/gui/share.png");
 }
