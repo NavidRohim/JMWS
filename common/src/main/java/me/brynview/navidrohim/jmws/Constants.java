d8fe765bfbb8eb2581d99497ca187b7365e267b5
Navid <navidanjumsamirohim@gmail.com>
2026-05-19 16:28:39 +0100

    Somehow forgot to change the server version

--- common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 
     public static final String MODID = "jmws";
     public static final String VERSION = "1.2.7-26.1.2"; // This is purely for display and is not needed
-    public static final double SERVER_VERSION = 1.13;
+    public static final double SERVER_VERSION = 1.14;
     public static final int JOURNEYMAP_LOCAL_SERVER_WAYPOINTS_BETA = 77;
     public static final boolean DEBUG = VERSION.contains("-beta.");
 
