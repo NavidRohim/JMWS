cc8661e010a227a7f890e63e2bb37a19a6a7f541
Navid <navidanjumsamirohim@gmail.com>
2026-07-10 20:26:53 +0100

    1.2.8 for 1.21.11, fix fatal bug with stable release versions of JM

--- common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     }
 
     public static final String MODID = "jmws";
-    public static final String VERSION = "1.2.7-1.21.11"; // This is purely for display and is not needed
+    public static final String VERSION = "1.2.8-1.21.11"; // This is purely for display and is not needed
     public static final double SERVER_VERSION = 1.14;
     public static final int JOURNEYMAP_LOCAL_SERVER_WAYPOINTS_BETA = 75;
     public static final boolean DEBUG = VERSION.contains("-beta.");
